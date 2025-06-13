92770979dae43d3038906eb9d23daa043b2af13d
okkkku <kyook0129@gmail.com>
2025-06-13 11:56:59 +0900

    update

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ async def root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": qa}
