3fec1a76c67b7496e233c3de6d297661997a99e4
okkkku <kyook0129@gmail.com>
2025-06-19 21:29:34 +0900

    update

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def index():
                                 ctx.moveTo(250, 250);
                                 ctx.arc(250,250,100,i*Math.PI/5,(i+1)*Math.PI/5,false);
                                 ctx.closePath();
-                                ctx.fillStyle = `rgb(${Math.floor(i*255/10)}, 255, 0)`;
+                                ctx.fillStyle = `rgb(${Math.floor(i*255/10)}, 0, 0)`;
                                 ctx.fill();
                             }
                         }
